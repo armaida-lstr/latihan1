282a24079dbb0933dd4d49a2d5ec9b0c2f229d94
LSTRarmaida <>
2022-03-08 01:49:39 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,142 @@
+// print("Hello World!")
+
+//let=nilai paten
+//var=bisa berubah
+
+// var winrate: Double = 48.36
+// print(winrate)
+
+// let 👍: String = "kkk"
+// print(👍)
+
+// let heroname: String = "ggg"
+// print(heroname)
+
+
+
+
+
+//boolean operator: AND(&&) OR(!!) NOT(!)
+
+// var isRain = true
+// if isRain{
+//   print("bring your umbrella")
+// }
+
+
+
+
+// print("aaaa")
+// var temperature =100
+
+// if temperature >= 65 && temperatute <= 75 {     print("temperature is okay")           }else if temperatur < 65{ print("it is too cold")          }else{
+//   print("it is too hot ")
+// }
+
+
+
+// if temperature >= 100 {     print("the water is boiling")                   }else{
+//   print("the water is not boilinng ")
+// }
+
+
+
+//switch-case
+// var numbofwheels=2
+
+// case 0:
+// print("missing wheels")
+// case 1:
+// print("unicycle")
+// case 2:
+// print("bycycle")
+// default:
+// print("thats a lot of wheels")
+
+
+// //macam-mcam int
+// print(Int8.max)
+// print(Int16.max)
+// print(Int32.max)
+// print(Int.max)
+// print(Int64  .max)
+// print(Int8.min)
+
+
+
+
+
+//range operator
+
+// let distance: Int =250
+
+// switch distance{
+//   case 0...9:
+//  print("your destination is close")
+//   case 10...99:
+//  print("your destination is not that far")
+//   case 100...999
+//  print("your destination is far from here")
+//   default: print("where are you going")
+
+  //case 0...10 artinya range 0 sampai 10
+  
+//}
+
+
+
+
+// let letter:Character ="u"
+
+// switch letter{
+//   case "a", "i", "u", "e", "o":
+//   print("the letter is a vowel")
+//   default:
+//   print:
+//   prit("the letter is consonant")
+// }
+
+
+
+//ternary operator
+
+// var largestnumb:Int
+// let num1 =  40
+// let num2 =10
+
+// if num1>num2{
+//   largestnumb = num1
+// }else{
+// largestnumb = num2
+// }
+
+
+//cara 2
+// largestnumb = num1 > num2 ? num1 : num2
+// print("largestnumb")
+//cara baca kondisi 2 setelah ? adalah true kondisi : false kondisi
+
+
+//cara3
+// let temp = 70
+// var result:String
+
+// result= (temp >= 65) && (temp <= 75) ? "temp is ok": "temp too hot/cold"
+// print("result")
+
+
+var largestnum: Int
+
+let num1 = 30
+let num2 = 10
+
+largestnum = (num1 > num2) ? num1 : num2
+
+print("largestnum is \(largestnum)")
+
+
+
+//Math operation : *, +,-, %
+//boolean operation:&&,||,!
+//ternary operators: ( ?:)
+
